d6884c8cc3c07a956935b679c6d1fb80c023cf8d
Federico Feroldi <federico@teamdigitale.governo.it>
2017-02-20 17:27:40 +0100

    Aggiunta minificazione HTML

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var jpegtran = require('imagemin-jpegtran');
 var gifsicle = require('imagemin-gifsicle');
+var minifyHTML = require('gulp-minify-html');
 
 gulp.task('jekyll', function() {
   return gulp.src('index.html', {
@@ -20,6 +21,14 @@ gulp.task('optimize-images', function () {
 			progressive: false,
 			svgoPlugins: [{removeViewBox: false}],
 			use: [pngquant(), jpegtran(), gifsicle()]
+		}))
+		.pipe(gulp.dest('_site/'));
+});
+
+gulp.task('optimize-html', function() {
+	return gulp.src('_site/**/*.html')
+		.pipe(minifyHTML({
+			quotes: true
 		}))
 		.pipe(gulp.dest('_site/'));
 });
