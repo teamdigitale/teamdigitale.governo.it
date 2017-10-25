826ad7678cfd096c939d338440b1dc8e70ef2ac6
Vito Falco <vitofalco@gmail.com>
2017-10-25 17:23:24 +0200

    New stuffs for this task

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -24,6 +24,22 @@ module LiquidFilters
     input.sort_by { |i| i[fullname_field].downcase.split(/ /)[1] }
   end
 
+  # Converts an integer to a string containing commas every three digits.
+  # Optionally supports a delimiter override for commas.
+  def intcomma(value, delimiter=",")
+      begin
+        orig = value.to_s
+        delimiter = delimiter.to_s
+      rescue Exception => e
+        puts "#{e.class} #{e}"
+        return value
+      end
+
+      copy = orig.strip
+      copy = orig.gsub(/^(-?\d+)(\d{3})/, "\\1#{delimiter}\\2")
+      orig == copy ? copy : intcomma(copy, delimiter)
+    end
+
 end
 
 Liquid::Template.register_filter(LiquidFilters)
