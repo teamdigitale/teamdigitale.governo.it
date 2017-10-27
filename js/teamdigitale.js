55fca59b84e8e2b2d0cc3c54479a8fd76a997290
Vito Falco <vitofalco@gmail.com>
2017-10-27 16:22:24 +0200

    Smooth transition when using the Toc's anchors fix #304 (#378)

--- js/teamdigitale.js
+++ js/teamdigitale.js
@@ -63,4 +63,25 @@ $(function() {
 		innerWrapperSelector: '.section-nav'
   	});
 
+  	// Smooth TOC
+	$('.section-nav a[href*="#"]').click(function(event) {
+		var deltaHeight = 50;
+	    if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	      
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	     
+	      if (target.length) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top - deltaHeight
+	        }, 1000, function() {
+	          // Callback 
+	          var $target = $(target);
+	          $target.focus();
+	        });
+	      }
+	    }
+	  });
+
 });
