7d72781dc154bca0ed71ba8210d6bf106cb60ae0
Federico Feroldi <federico@teamdigitale.governo.it>
2017-12-13 10:50:49 +0100

    Fixes #144, Deploy bot should share the link to the triggered build

--- _contrib/deploy-bot/skills/deploy.js
+++ _contrib/deploy-bot/skills/deploy.js
@@ -4,53 +4,48 @@
  * TODO automatically build the project list.
  */
 
-const CircleCI = require('circleci');
+const CircleCI = require('circleci')
 
 const sites = {
 
   teamdigitale: {
     username: 'teamdigitale',
     project: 'teamdigitale.governo.it',
-    branch: 'master',
+    branch: 'master'
   }
 
 }
 
 const ci = new CircleCI({
-  auth: process.env.CIRCLECI_TOKEN,
-});
+  auth: process.env.CIRCLECI_TOKEN
+})
 
-const sites_list = Object.keys(sites).map((s) => '`' + s + '`' ).join(', ');
+const sitesList = Object.keys(sites).map((s) => '`' + s + '`').join(', ')
 
-module.exports = function(controller) {
-
-  controller.hears(['^help'], 'direct_message,direct_mention', function(bot, message) {
-
-    bot.createConversation(message, function(err, convo) {
+module.exports = function (controller) {
+  controller.hears(['^help'], 'direct_message,direct_mention', function (bot, message) {
+    bot.createConversation(message, function (err, convo) {
       if (!err) {
-        convo.say('To deploy a site jusy say `deploy SITENAME` where `SITENAME` is one of: ' + sites_list);
-        convo.activate();
+        convo.say('To deploy a site jusy say `deploy SITENAME` where `SITENAME` is one of: ' + sitesList)
+        convo.activate()
       }
-    });
+    })
+  })
 
-  });
+  controller.hears(['^deploy( (.*))?'], 'direct_message,direct_mention', function (bot, message) {
+    const siteLabel = message.match[2]
 
-  controller.hears(['^deploy( (.*))?'], 'direct_message,direct_mention', function(bot, message) {
-    const site_label = message.match[2];
-
-    if (site_label && sites[site_label]) {
-      bot.reply(message, `I'm going to queue a deploy of \`${site_label}\` now`);
+    if (siteLabel && sites[siteLabel]) {
+      bot.reply(message, `I'm going to queue a deploy of \`${siteLabel}\` now`)
       ci.startBuild({
-        username: sites[site_label].username,
-        project: sites[site_label].project,
-        branch: sites[site_label].branch,
+        username: sites[siteLabel].username,
+        project: sites[siteLabel].project,
+        branch: sites[siteLabel].branch
       }).then((build) => {
-        bot.reply(message, `I've queued a new build for \`${site_label}\``);
-      });
+        bot.reply(message, `I've queued a new build for \`${siteLabel}\`, build status at ${build.build_url}`)
+      })
     } else {
-      bot.reply(message, 'To deploy a site jusy say `deploy SITENAME` where `SITENAME` is one of: ' + sites_list);
+      bot.reply(message, 'To deploy a site jusy say `deploy SITENAME` where `SITENAME` is one of: ' + sitesList)
     }
-
-  });
-
-};
+  })
+}
